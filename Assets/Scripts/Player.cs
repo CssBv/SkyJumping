42f21dcb20750e9169477aefcceb324124739f52
Cassandra Barreto <cassandrabarrreto@MacBook-Pro-de-Cassandra.local>
2021-04-17 17:45:37 -0500

    Added Parallex Logic and player logic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    /*Setting Jumping speed of the player*/
+    private float jumpSpeed = 3f;
+
+    /*Setting rigidbody of character*/
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        /*Obtain Rigidbody Component */
+        rb.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    /*Detect touch*/
+    void FixedUpdate()
+    {   
+
+        /*If screen is touched then make character to jump*/
+        if(Input.GetMouseButtonDown(0)) //--> Touch screen: Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began ----- Input.GetKeyDown(KeyCode.Space )
+        {   
+            Debug.Log("");
+
+            /*Add force to the rigidbody. AddForce takes two arguments: Vector2 and Force meode. In this case, takes Impulse. Create Vector2 that
+            sets x and y as the speed of the jump. Force equals to mass * acceleration*/
+            rb.AddForce( new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+        }      
+    }
+}
---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{   
+    /*Create box collider variable*/
+    public BoxCollider2D boxCollider;
+
+    /*Create Rigidbody variable*/
+    public Rigidbody2D rb;
+
+
+    private float width; 
+
+    private float scrollSpeed = -5;
+
+d Dk
+    void Start()
+    {   
+        /*Get Collider from gameobject*/
+        boxCollider = GetComponent<BoxCollider2D>();
+        /*Get rigidbody from component*/
+        rb = GetComponent<Rigidbody2D>();
+
+        /*Obtain width with collider*/
+        width = boxCollider.size.x;
+
+        /*Disable collider's calculations*/
+        boxCollider.enabled = false;
+
+        /*Position and direction of rigidbody with Vector2. x as first paremeter, y as second  parameter. Scroll image to the left*/
+        rb.velocity = new Vector2(scrollSpeed, 0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        /*If position in x is less than  negative width*/
+        if(transform.position.x < -width)
+        {   
+            /*Reset background's position by creating vector 2 that has as argument x  (multuplies width * 2 (background twin )in x position)  and 0 in y */
+            Vector2 resetPosition = new Vector2(width * 2f, 0);
+            /*Store the position of gameobject's transform. Equal it to transform.position plus resetPosition and cast the sum into a Vector2*/
+            transform.position = (Vector2)transform.position + resetPosition;
+        }
+    }
+}
+ 
